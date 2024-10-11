ddb331a5234bd77b039c227b472c3006ea2ba8bb
BRicsi <barnari@KKSZKI.LOCAL>
2024-10-11 11:08:07 +0200

    Min max result is created.

--- Program.cs
+++ Program.cs
@@ -10,7 +10,41 @@ namespace MinMax
     {
         static void Main(string[] args)
         {
+            int szamok = 10;
+            int min = 1;
+            int max = 100;
 
+            Random random = new Random();
+            int[] tomb = new int[szamok];
+
+            for (int i = 0; i < szamok; i++)
+            {
+                tomb[i] = random.Next(min, max + 1);
+            }
+
+            int legkisebb = tomb[0];
+
+            for (int i = 1; i < tomb.Length; i++)
+            {
+                if (tomb[i] < legkisebb)
+                {
+                    legkisebb = tomb[i];
+                }
+            }
+
+            int legnagyobb = tomb[0];
+
+            for (int i = 1; i < tomb.Length; i++)
+            {
+                if (tomb[i] > legnagyobb)
+                {
+                    legnagyobb = tomb[i];
+                }
+            }
+
+            Console.WriteLine("A tömbnek az elemei: " + string.Join(", ", tomb));
+            Console.WriteLine("A legkisebb elem: " + legkisebb);
+            Console.WriteLine("A legnagyobb elem: " + legnagyobb);
         }
     }
 }
